d0b28cb7c83319a3525ba36432c0de3c25463f3f
Zachary Hardin <zacharyhardin@Zacharys-MacBook-Pro-4.local>
2018-01-29 19:30:36 -0600

    Add one data cell to table view controller.

--- Checklists/Checklists/ChecklistViewController.swift
+++ Checklists/Checklists/ChecklistViewController.swift
@@ -16,4 +16,13 @@ class ChecklistViewController: UITableViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ChecklistItem", for: indexPath)
+        return cell
+    }
 }
