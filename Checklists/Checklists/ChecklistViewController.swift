3857bc4661dc10d5a90f7a6372fa482f20aa44ac
Zachary Hardin <zacharyhardin@Zacharys-MacBook-Pro-4.local>
2018-01-30 18:56:55 -0600

    Add five rows with static data.

--- Checklists/Checklists/ChecklistViewController.swift
+++ Checklists/Checklists/ChecklistViewController.swift
@@ -18,11 +18,26 @@ class ChecklistViewController: UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return 5
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ChecklistItem", for: indexPath)
+        
+        let label = cell.viewWithTag(1000) as! UILabel
+        
+        if indexPath.row == 0 {
+            label.text = "Read Lorien Lagacies"
+        } else if indexPath.row == 1 {
+            label.text = "Take out your contacts"
+        } else if indexPath.row == 2 {
+            label.text = "Learn iOS development"
+        } else if indexPath.row == 3 {
+            label.text = "Practice guitar"
+        } else if indexPath.row == 4 {
+            label.text = "Tune drum kit"
+        }
+        
         return cell
     }
 }
